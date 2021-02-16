eea9becd2e2e37cbc74d04d2c0c73dff18b23b8e
Duy Truong <duytruongh@gmail.com>
2021-02-15 19:14:57 -0800

    02152021 renamed login button modal

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 /* Modal Start */
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("loginModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var loginButton = document.getElementById("loginButton");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+loginButton.onclick = function() {
   modal.style.display = "block";
 }
 
